7b811afed5a19fe2af3c3a21b86d2a9383f048ba
aParrot <a.lapenas04@gmail.com>
2023-11-06 19:36:23 +0100

    Update metadata and navbar layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,34 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <nav className="flex flex-row justify-between gap-4">
-        {/* <Image src={Logo} alt="logo" className="w-32" /> */}
-        <ul className="flex flex-row gap-4">
+      <nav className="flex flex-row gap-4 justify-between items-center w-full px-10 py-2 absolute z-50">
+        <Image
+          src="/static/images/logo.svg"
+          alt="logo"
+          height={71}
+          width={101}
+        />
+        <ul className="flex flex-row gap-4 items-center">
           <li>
-            <a href="#">Om oss</a>
+            <a href="#hero">Hjem</a>
           </li>
           <li>
-            <a href="#">Kontakt</a>
+            <a href="#menu">Meny</a>
+          </li>
+          <li>
+            <a href="#contact">Kontakt oss</a>
+          </li>
+          <li>
+            <Button>Bestill</Button>
           </li>
         </ul>
       </nav>
       {/* Hero */}
-      <section className="flex flex-row h-[90vh] w-full flex-1">
-        <div className="w-full flex flex-col items-start gap-4 mt-[45vh] p-10 ml-auto">
+      <section className="flex flex-row h-[80vh] w-full flex-1">
+        <div className="w-full flex flex-col items-start gap-4 my-auto translate-y-10 px-10 ml-auto">
           <h1 className="text-5xl font-serif font-light">
             Opplev Smaken av Fantastisk Sushi
           </h1>
