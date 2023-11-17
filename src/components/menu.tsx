a473327ff068e2db3b7419b81fbdbcaf82bf8b74
aParrot <a.lapenas04@gmail.com>
2023-11-17 23:34:50 +0100

    Update menu component layout and styling

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -8,21 +8,20 @@ import { Button } from "@/components/ui/button";
 export default function Menu({ pages }) {
   const [selectedPage, setSelectedPage] = useState(pages[0]);
 
+  console.log(selectedPage.subPages.length);
+
+  const subPageCount = selectedPage.subPages.length;
   return (
     <section
       id="menu"
       className="w-full px-[70px] pb-[50px] flex-col justify-start items-start inline-flex"
     >
       <h2 className="self-stretch text-center text-foreground text-3xl font-bold font-['Cormorant Garamond']">
-        Menu
+        Meny
       </h2>
       <div className="self-stretch pt-4 justify-center items-center gap-[30px] inline-flex">
         {pages.map((page) => {
-          let isPressed = false;
-          if (page.name === selectedPage.name) {
-            console.log(selectedPage);
-            isPressed = true;
-          }
+          const isPressed = page.name === selectedPage.name;
           return (
             <Button
               variant={"outline"}
@@ -37,38 +36,36 @@ export default function Menu({ pages }) {
           );
         })}
       </div>
-      <div className="self-stretch pt-[15px] flex-col justify-start items-start gap-[30px] flex">
+      <div
+        className={`grid grid-cols-${subPageCount} place-content-center w-full pt-[15px] gap-[30px] mt-8`}
+      >
         {selectedPage.subPages.map((page, subPageIdx) => (
           <div
             key={subPageIdx}
-            className="self-stretch flex-col justify-start items-start gap-2.5 flex"
+            className="flex flex-col gap-2.5 min-w-[250px]:"
           >
             <h3 className="text-2xl font-bold">{page.name}</h3>
-            <div className="overflow-x-scroll self-stretch justify-start items-center gap-[50px] inline-flex">
+            <div className="flex flex-col gap-6">
               {page.dishes.map((dish, index) => (
                 <div
                   key={index}
-                  className="min-w-[200px] p-2.5 rounded-xl border border-foreground flex-col justify-start items-start gap-0.5 flex"
+                  className="flex flex-col self-stretch min-w-[250px] p-2.5 rounded-xl border border-foreground/30 gap-0.5"
                 >
-                  <Image
-                    className="self-stretch rounded-xl"
-                    src="https://via.placeholder.com/189x186"
-                    alt="sushi"
-                    width={189}
-                    height={186}
-                  />
-                  <p className="text-foreground text-base font-bold font-sans">
-                    {dish.name}
-                  </p>
-                  <div className="justify-start items-center inline-flex">
-                    <span className="text-foreground text-base font-bold font-sans">
-                      {dish.price}
-                    </span>
-                    <span className="text-foreground text-base font-bold font-sans">
-                      kr
-                    </span>
+                  <div className="flex flex-row justify-between">
+                    <p className="text-foreground text-base font-bold font-sans">
+                      {dish.name}
+                    </p>
+                    <div className="grow border-dotted border-b-2 mx-2 border-black" />
+                    <div className="justify-start items-center inline-flex">
+                      <span className="text-foreground text-base font-bold font-sans">
+                        {dish.price}
+                      </span>
+                      <span className="text-foreground text-base font-bold font-sans">
+                        kr
+                      </span>
+                    </div>
                   </div>
-                  <p className="self-stretch text-foreground text-base font-normal font-sans">
+                  <p className="self-stretch text-foreground text-base font-normal font-sans line-clamp-3">
                     {dish.description}
                   </p>
                 </div>
